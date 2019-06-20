39b8298485c9afec4f829198fd616952f360c1fa
lennyjuma <lennyjuma34@gmail.com>
2019-06-20 13:38:04 -0700

    first commit

---
+++ src/components/BasicComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "BasicComponent.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "LoginForm.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
